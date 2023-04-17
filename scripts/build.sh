13e445be78f5585fa21bf6f0924975dab60215d4
Milos Nikic <milos.nikic@gmail.com>
2023-04-17 13:32:56 +0200

    Deploy gh action

---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+echo "Application is being built..."
+npm run build
+echo "Copying index.html to ./dist..."
+cp index.html ./dist
+echo "Copying styles.css to ./dist..."
+cp src/css/* ./dist
+
+
+cd dist
+echo "Correct paths in index.html"
+sed -i.back 's/\/dist//g' index.html
+sed -i.back 's/.\/src\/css\///g' index.html
+echo "Build done"
